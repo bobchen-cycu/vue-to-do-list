5d01a5b90ff712865637ad40ab123e09c804c1e7
bobchen <bobchen0219@cycu.org.tw>
2019-09-17 17:12:06 +0800

    bobchen

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    todos:[
+        {text:  "the first item.",done: true },
+        {text:  "the second one.",done: false },
+        {text:  "the third one." ,done: false }
+    ]
   }
 })
